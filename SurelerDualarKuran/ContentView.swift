cce2d8c5194c0255e346bd4fe61428a48c499ba7
inceselim <inceselim91@gmail.com>
2026-04-20 17:00:52 +0300

    Initial Commit

---
+++ SurelerDualarKuran/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SurelerDualarKuran
+//
+//  Created by Selim ince on 20.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SurelerDualarKuran/SurelerDualarKuranApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurelerDualarKuranApp.swift
+//  SurelerDualarKuran
+//
+//  Created by Selim ince on 20.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct SurelerDualarKuranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
