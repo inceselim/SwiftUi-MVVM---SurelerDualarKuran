4daecf988f73081469d27b5601990cd3df5a7bb2
inceselim <inceselim91@gmail.com>
2026-04-21 23:27:37 +0300

    Elif Be Eklendi

---
+++ SurelerDualarKuran/Views/DuaDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DuaDetailView.swift
+//  SurelerDualarKuran
+//
+//  Created by Selim ince on 21.04.2026.
+//
+
+import SwiftUI
+
+struct DuaDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DuaDetailView()
+}
